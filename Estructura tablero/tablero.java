b18458dbaaa944fea3759d5d1b08e7321457fdbc
repl.it user <replituser@example.com>
2020-06-14 05:29:00 +0000

    Como hacer una matriz

---
+++ Estructura tablero/tablero.java
@@ -0,0 +1,21 @@
+
+
+public class Ejercicio_1{
+
+
+
+  public static void man(String[]args{
+    int matriz[] [] = new int [9] [9];
+
+    for(int i=0;i<matriz.length;i++){
+      for(int j=0;j<matriz[0].length;j++){
+        matriz<[i][j]=
+        System.out.print(matriz[i][j]+" ");
+      }
+    }
+
+
+
+
+  }
+}
